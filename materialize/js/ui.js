63403fbe94b5e3af03677d8bf70344e3e61464c5
SeanglongLim <167268744+Limseanglong9678@users.noreply.github.com>
2024-12-09 21:17:42 -0600

    Delete materialize directory

--- materialize/js/ui.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){
-    const menus = document.querySelector(".sidenav");
-    M.Sidenav.init(menus,{edge: "right"})
-
-    const forms = document.querySelector(".side-forms");
-    M.Sidenav.init(menus,{edge: "left"})
-});
